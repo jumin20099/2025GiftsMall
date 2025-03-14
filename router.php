a6e583f8bb3fbe924e7a22bc0da13316e2274507
김주민 <jumin20099@gmail.com>
2025-03-14 23:27:57 +0900

    기타 라우팅 변경

--- router.php
+++ router.php
@@ -1,19 +1,28 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "giftsmall");
 if ($conn->connect_error) {
-    exit("DB 연결 실패ㅅㄱ");
+    exit("DB 연결 실패");
 }
 
 session_start();
 
-$request = $_SERVER['REQUEST_URI'];
-$path = explode("?", $request);
-$path[1] = isset($path[1]) ? $path[1] : null;
-$resource = explode("/", $path[0]);
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$base_path = str_replace('/router.php', '', $request_uri);
+$route = trim($base_path, '/');
+
+// URL에 ?가 포함된 경우 page 값 추출
+if (!empty($_SERVER['QUERY_STRING'])) {
+    parse_str($_SERVER['QUERY_STRING'], $query_params);
+    if (isset($query_params['page'])) {
+        $route = $query_params['page'];
+    }
+}
+
+
 $pages = "";
 
-
-switch ($resource[1]) {
+switch ($route) {
+    case '':
     case 'main':
         $pages = './pages/index.php';
         break;
@@ -41,17 +50,20 @@ switch ($resource[1]) {
     case 'loginProcess':
         $pages = './pages/login_process.php';
         break;
-    case 'manageNotice':
-        $pages = './pages/manageNotice.php';
-        break;
     case 'manageProducts':
         $pages = './pages/manageProducts.php';
         break;
-
+    case 'manageNotice':
+        $pages = './pages/manageNotice.php';
+        break;
+    case 'logout':
+        $pages = './pages/logout.php';
+        break;
     default:
-        echo "ㄴㄴ";
+        echo "잘못된 경로입니다.";
         exit;
 }
+
 include("./components/header.php");
 include($pages);
 include("./components/footer.php");
